2f40b30689db722f0206d3a90672bf0611d39281
x7x9x777 <x7x9x777@gmail.com>
2025-02-25 19:36:46 +0200

    css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // import { useState } from 'react'
 import { Header, Hero, About, Projects, Footer } from './components/root';
+import './App.css'
+
 
 export default function App() {
   
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,37 @@
-
-
 export default function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section>
+      <h1>x7x9x777</h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu turpis
+        sit amet lorem tempor mollis. Proin ultrices enim ante, et consequat
+        turpis fringilla sit amet. Ut nec maximus nulla, a ultrices arcu. Sed
+        fermentum tristique lorem, aliquet semper ligula mollis quis. Sed
+        commodo augue nisl, eget dignissim mi auctor at. Nam eleifend varius
+        sagittis. Sed semper tristique ornare. Nam risus justo, consequat sit
+        amet faucibus ut, faucibus sit amet nibh.
+      </p>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu turpis
+        sit amet lorem tempor mollis. Proin ultrices enim ante, et consequat
+        turpis fringilla sit amet. Ut nec maximus nulla, a ultrices arcu. Sed
+        fermentum tristique lorem, aliquet semper ligula mollis quis. Sed
+        commodo augue nisl, eget dignissim mi auctor at. Nam eleifend varius
+        sagittis. Sed semper tristique ornare. Nam risus justo, consequat sit
+        amet faucibus ut, faucibus sit amet nibh.
+      </p>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu turpis
+        sit amet lorem tempor mollis. Proin ultrices enim ante, et consequat
+        turpis fringilla sit amet. Ut nec maximus nulla, a ultrices arcu. Sed
+        fermentum tristique lorem, aliquet semper ligula mollis quis. Sed
+        commodo augue nisl, eget dignissim mi auctor at. Nam eleifend varius
+        sagittis. Sed semper tristique ornare. Nam risus justo, consequat sit
+        amet faucibus ut, faucibus sit amet nibh.
+      </p>
+    </section>
+  );
 }
