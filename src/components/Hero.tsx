3f23f40e38a0e0b38420a4f1c2320664623f1e0e
x7x9x777 <x7x9x777@gmail.com>
2025-03-02 15:03:38 +0200

    framer added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,46 @@
+import { motion } from "framer-motion"
+
 export default function Hero() {
   return (
-    <section>
+    <>
+     <section>
+    <h1>x7x9x777</h1>
+    <p>
+      {" "}
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu turpis
+      sit amet lorem tempor mollis. Proin ultrices enim ante, et consequat
+      turpis fringilla sit amet. Ut nec maximus nulla, a ultrices arcu. Sed
+      fermentum tristique lorem, aliquet semper ligula mollis quis. Sed
+      commodo augue nisl, eget dignissim mi auctor at. Nam eleifend varius
+      sagittis. Sed semper tristique ornare. Nam risus justo, consequat sit
+      amet faucibus ut, faucibus sit amet nibh.
+    </p>
+    <p>
+      {" "}
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu turpis
+      sit amet lorem tempor mollis. Proin ultrices enim ante, et consequat
+      turpis fringilla sit amet. Ut nec maximus nulla, a ultrices arcu. Sed
+      fermentum tristique lorem, aliquet semper ligula mollis quis. Sed
+      commodo augue nisl, eget dignissim mi auctor at. Nam eleifend varius
+      sagittis. Sed semper tristique ornare. Nam risus justo, consequat sit
+      amet faucibus ut, faucibus sit amet nibh.
+    </p>
+    <p>
+      {" "}
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu turpis
+      sit amet lorem tempor mollis. Proin ultrices enim ante, et consequat
+      turpis fringilla sit amet. Ut nec maximus nulla, a ultrices arcu. Sed
+      fermentum tristique lorem, aliquet semper ligula mollis quis. Sed
+      commodo augue nisl, eget dignissim mi auctor at. Nam eleifend varius
+      sagittis. Sed semper tristique ornare. Nam risus justo, consequat sit
+      amet faucibus ut, faucibus sit amet nibh.
+    </p>
+  </section>
+    <motion.section
+  animate={{ y: -1000 }}
+  transition={{ ease: "easeOut", duration: 2 
+  }}
+/>
       <h1>x7x9x777</h1>
       <p>
         {" "}
@@ -32,6 +72,9 @@ export default function Hero() {
         sagittis. Sed semper tristique ornare. Nam risus justo, consequat sit
         amet faucibus ut, faucibus sit amet nibh.
       </p>
-    </section>
+    <motion.section/>
+   
+  </>
+
   );
 }
