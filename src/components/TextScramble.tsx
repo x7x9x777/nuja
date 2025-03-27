6d349280907e0661b8a9fef92142d1b9e400af46
x7x9x777 <x7x9x777@gmail.com>
2025-03-27 21:54:50 +0200

    scrumble all at once

--- src/components/TextScramble.tsx
+++ src/components/TextScramble.tsx
@@ -3,28 +3,81 @@ import { useEffect, useState } from "react";
 
 
 export default function TextScramble ({text}:{text:string}) {
-
-const chars = "abcdefghijklmnop"    
+const string = "abcdefghijklmnop"   
+const chars =  string.split("")
 const content = text.split("")
 const [mytext,setMytext] = useState(text)
 
 function hover(content:any){
- for(let i=0; i<=content.length;i++){
-    const newcontent=text.split("")
-    setInterval(()=>myCallback(i),5
+  let intervalCount = 0
+const interval = setInterval(()=>{
+const newText=content.split("").map(
+  // (item: string)=>item!==" "?item=chars[Math.floor(Math.random() * chars.length )]: item=" "
+(item:string,index:any)=>{
+  if(item===" "){return item=" "}
 
-    )
-  function myCallback(i:any){
-  newcontent[i]= chars[Math.floor(Math.random() * (chars.length ))];
-  setMytext(newcontent.join(""))
-  }
+  if(intervalCount<=9){
+    return item=chars[Math.floor(Math.random() * chars.length )]
+   
 
- }
+  
+  } else{ setTimeout(()=>{
+    return item=text[index]
+  },100)
+  
+  // (1+index)*1000) 
+
+}
+
+
+
+ 
+  
+
+
+
+
+
 
 
 
 
 }
+
+
+
+)
+setMytext(newText)
+
+intervalCount+=1
+if(intervalCount>=10){
+  setMytext(text)
+  clearInterval(interval)
+}
+},30)
+//  for(let i=0; i<content.length;i++){
+//     const newcontent=content.split("")
+   
+//    newcontent[i]!==" "?myCallback():  setMytext(newcontent.join(""))
+
+    
+//   function myCallback(){
+//     console.log(i)
+//   newcontent[i]= chars[Math.floor(Math.random() * chars.length )];
+//   console.log(newcontent.join(""))
+//   setMytext(newcontent.join(""))
+  
+//   }
+
+//  }
+
+
+
+
+}
+
+
+
 
  
 
